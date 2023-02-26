faae4636481cbc66c1aa8073406315408e5e4c8a
Nitin Dinnu <68476175+nitindinnu@users.noreply.github.com>
2023-02-26 12:21:33 +0530

    .

---
+++ gates/and_gate/and_dut.do
@@ -0,0 +1,11 @@
+vlib work
+vlog and_dut.v +acc
+vsim work.and_tb 
+add wave -r *
+run -all
+
+# vlib work
+# vlog jerky.v tb_jerky.v +acc -->vlog <lead dut><top dut name > <test bench> +acc
+# vsim work.tb 
+# add wave -r *
+# run -all
--- gates/and_gate/and_dut.v
+++ gates/and_gate/and_dut.v
@@ -7,3 +7,22 @@ module and_dut (
     and(y,a,b);
     //assign y=a&b;
 endmodule
+
+module and_tb (
+);
+    reg p,q; //inputs
+    wire r;  //outputs
+
+    and_dut test(.a(p),.b(q),.y(r));
+
+    initial begin
+    $monitor("p=%0b,q=%0b,r=%0b",p,q,r);
+    // $display("p=%0b,q=%0b,r=%0b",p,q,r);
+    p=0;q=0; #20;
+    p=0;q=1; #20;
+    p=1;q=0; #20;
+    p=1;q=1; #20;
+    $stop;
+    end
+endmodule
+
